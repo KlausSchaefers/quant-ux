f7da5b72dd740d651af457a8a05ee21fefa27c3d
Klaus <Klaus.Schaefers@gmail.com>
2019-11-20 22:04:59 +0100

    Improved DataView

--- src/canvas/DataView.vue
+++ src/canvas/DataView.vue
@@ -3,6 +3,7 @@ import CheckBox from 'common/CheckBox'
 import lang from 'dojo/_base/lang'
 import on from 'dojo/on'
 import css from 'dojo/css'
+import RestEngine from 'core/RestEngine'
 
 export default {
     name: 'DataView',
@@ -41,8 +42,8 @@ export default {
         },
         createWidgetDataView (widget, div) {
             if (this.hasDataView && widget) {
-                var dataBinding = this.getDataBinding(widget);
-                if (dataBinding) {
+                var dataBindings = this.getAllDataBinding(widget);
+                if (dataBindings && dataBindings.length > 0) {
                     // console.debug('createWidgetDataView', widget.id, dataBinding)
                     if (!this._dataViewDivs) {
                         this._dataViewDivs = {}
@@ -59,16 +60,46 @@ export default {
                     css.add(div, 'MatcCanvasDataViewLabelCntr')
                     let dataDiv = document.createElement('div')
                     css.add(dataDiv, 'MatcCanvasDataViewLabel')
-                    if (dataBinding && Object.keys(dataBinding).length > 0){
-                        let txt = Object.values(dataBinding).join(', ')
-                        dataDiv.innerHTML = txt
-                    }
+                    let txt = dataBindings.join(', ')
+                    dataDiv.innerHTML = txt
                     div.appendChild(dataDiv)
                     this._dataViewDivs[widget.id] = dataDiv
                 }
                 
             }
         },
+
+        getAllDataBinding (widget) {
+            let result = []
+            let dataBinding = this.getDataBinding(widget)
+            if (dataBinding) {
+                result = Object.values(dataBinding)
+            }
+             /**
+             * Rest needs special handling
+             */
+            if (widget.props && widget.props.rest) {
+                let rest = widget.props.rest
+                let restBindings = RestEngine.getNeededDataBings(rest)
+                result = result.concat(restBindings)
+                if (rest.output && rest.output.databinding) {
+                    result.push(rest.output.databinding)
+                }
+            }
+            /**
+             * Logic Widgets have the batabinding in the lines
+             */
+            if (widget.type === 'LogicOr') {
+                let fromLines = this.getFromLines(widget)
+                fromLines.forEach(line => {
+                    if (line.rule && line.rule.databinding) {
+                        result.push(line.rule.databinding)
+                    }
+                })
+            }
+            return result
+        },
+
         updateWidgetDataView (widget) {
             if (this.hasDataView && widget) {
                 let div = this.widgetDivs[widget.id]
