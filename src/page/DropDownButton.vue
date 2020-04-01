e14e5b125a28032b6930d59fa07974db2625d2cd
310267859 <klaus.schaefers@philips.com>
2020-04-01 22:06:59 +0100

    Moved Pics

--- src/page/DropDownButton.vue
+++ src/page/DropDownButton.vue
@@ -1,29 +1,32 @@
 
 <template>
-     <div class="MatcButton MatcDropDownButton MatcDropDownButtonWidth">
-        <div type="button" data-dojo-attach-point="button"> 
-            <label data-dojo-attach-point="label" class="MatcDropDownLabel"></label> 
-            <span class="caret"></span> 
-            </div>
-            <div class="MatcDropDownPopUp MatcDropDownButtonWidth" role="menu" data-dojo-attach-point="popup">
-                <ul class="" role="menu" data-dojo-attach-point="ul">
-                </ul>
-            </div>
-        </div>
+  <div class="MatcButton MatcDropDownButton MatcDropDownButtonWidth">
+    <div type="button" data-dojo-attach-point="button">
+      <label data-dojo-attach-point="label" class="MatcDropDownLabel"></label>
+      <span class="caret"></span>
+    </div>
+    <div
+      class="MatcDropDownPopUp MatcDropDownButtonWidth"
+      role="menu"
+      data-dojo-attach-point="popup"
+    >
+      <ul class role="menu" data-dojo-attach-point="ul"></ul>
+    </div>
+  </div>
 </template>
 <script>
-import DropDownButton from 'common/DropDownButton'
+import DropDownButton from "common/DropDownButton";
 
 export default {
-    name: 'DropDownButton',
-    mixins:[DropDownButton],
-    data: function () {
-        return {
-            openCSS: "MatcDropDownButtonOpen", 
-            iconCSS: "MatcDropDownIcon", 
-            labelCSS: "MatcDropDownLabel", 
-            selectedCSS: "MatcDropDownButtonSelected"
-        }
-    }
-}
+  name: "DropDownButton",
+  mixins: [DropDownButton],
+  data: function() {
+    return {
+      openCSS: "MatcDropDownButtonOpen",
+      iconCSS: "MatcDropDownIcon",
+      labelCSS: "MatcDropDownLabel",
+      selectedCSS: "MatcDropDownButtonSelected"
+    };
+  }
+};
 </script>
