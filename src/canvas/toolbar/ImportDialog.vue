a9ae42fff4d552fbe19bfc11e25155d55d88db68
310267859 <klaus.schaefers@gmail.com>
2020-07-06 11:55:17 +0100

    Removed stuff

--- src/canvas/toolbar/ImportDialog.vue
+++ src/canvas/toolbar/ImportDialog.vue
@@ -94,7 +94,6 @@ export default {
             progessPercent: 0,
             figmaAcccessKey: '',
             figmaUrl: '',
-            user: null,
             isPublic: false
         }
     },
