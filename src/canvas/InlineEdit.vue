43e6d2cc3da5a7863dfa7e9f8fde5dcaf2e08e2d
KlausSchaefers <klaus.schaefers@gmail.com>
2022-06-06 18:29:08 +0200

    Fix Some stuff

--- src/canvas/InlineEdit.vue
+++ src/canvas/InlineEdit.vue
@@ -115,8 +115,20 @@ export default {
 				}
 			}
 			this.cleanUpInlineEdit();
+			this.clearInlineEditSelections()
 		},
 
+		clearInlineEditSelections () {
+			if (window.getSelection) {
+				if (window.getSelection().empty) {  // Chrome
+					window.getSelection().empty();
+				} else if (window.getSelection().removeAllRanges) {  // Firefox
+					window.getSelection().removeAllRanges();
+				}
+				} else if (document.selection) {  // IE?
+				document.selection.empty();
+			}
+		},
 
 
 		inlineEditStarted  (){
