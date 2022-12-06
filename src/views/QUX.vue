d70e35166065b2f12b5933430c224c44fe76cbe0
KlausSchaefers <klaus.schaefers@gmail.com>
2022-12-06 19:40:36 +0100

    Small fix

--- src/views/QUX.vue
+++ src/views/QUX.vue
@@ -78,7 +78,7 @@ export default {
   async mounted() {
     this.logger = new Logger('QUX')
     this.user = Services.getUserService().load()
-    this.logger.log(-1, 'mounted', 'exit >> ' + this.user.email + " >> locale: " + navigator.language)
+    this.logger.log(-1, 'mounted', "locale: " + navigator.language)
     this.$root.$on('MatcLogout', (user) => {
         this.onLogout(user)
     })
