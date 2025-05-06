bd83d2c4e01a50b90c20a1aa327bdcaf6eae5c3a
KlausSchaefers <klaus.schaefers@gmail.com>
2025-05-06 23:08:01 +0100

    more

--- src/services/SymbolService.js
+++ src/services/SymbolService.js
@@ -140,10 +140,11 @@ class SymbolService extends AbstractService{
             import(/* webpackChunkName: "themes" */ 'themes/composite/toast_error.json'),
             import(/* webpackChunkName: "themes" */ 'themes/composite/toast_success.json'),
             import(/* webpackChunkName: "themes" */ 'themes/composite/card_l.json'),
+            import(/* webpackChunkName: "themes" */ 'themes/composite/card_l_space.json'),
             import(/* webpackChunkName: "themes" */ 'themes/composite/card_s_l.json'),
             import(/* webpackChunkName: "themes" */ 'themes/composite/card_s_r.json'),
             import(/* webpackChunkName: "themes" */ 'themes/composite/card_user.json'),
-
+            import(/* webpackChunkName: "themes" */ 'themes/composite/card_xl_button.json'),
 
             import(/* webpackChunkName: "themes" */ 'themes/survey/textbox.json'),
             // import(/* webpackChunkName: "themes" */ 'themes/survey/labeledtextbox.json'),
